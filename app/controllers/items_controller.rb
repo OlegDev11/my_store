4d62d3c647ac5fc5c7930d563b9606039f72eaeb
Oleg <mr.mastercredit@gmail.com>
2018-12-30 17:46:09 +0200

    Изменение в контроллере

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+#код повтояется, выносим его в один метод @item = Item.create(item_params)
+before_action :find_item,          only: [:show, :edit, :update, :destroy]
+#для запрета доступа пользователю к этим методам, но разрешить для админа
+before_action :check_if_admin,     only: [:edit, :update, :new, :cewate, :destroy]
 
   def index
     @items = Item.all                            #поиск в БД всех обектов
@@ -6,7 +10,7 @@ class ItemsController < ApplicationController
 
 #      /items       POST
    def create                                     #новый обект, который мы создаём и сохраняем в БД
-  @item = Item.create(item_params)
+   @item = Item.create(item_params)
    if @item.errors.empty?                         #проверяет есть ли ошибки
     redirect_to item_path(@item)                  # redirect_to делает ещё один запрос на сервер по пути item_path(items/:id)
   # render "show"
@@ -15,20 +19,16 @@ class ItemsController < ApplicationController
    end
  end
 
-#метод для сохранения параметров в БД
-  private def item_params
-     params.require(:item).permit(:price, :name, :weight, :description)
-   end
-
 #URL   /items/1      GET      HTTP метод
    def show                                           #то что выводит на экран после сохранения в БД
-     @item = Item.find(params[:id])
+#     @item = Item.find(params[:id])
+     unless @item
      render "items/show"
    end
 
 #     /items/1       PUT (всё равно что POST)
    def update                                         #обновляем форму
-     @item = Item.find(params[:id])                   #поиск товара
+  #   @item = Item.find(params[:id])                   #поиск товара
      @item.update_attributes(item_params)             #обновляем атрибуты
        if @item.errors.empty?
          redirect_to item_path(@item)
@@ -44,13 +44,28 @@ class ItemsController < ApplicationController
 
 #    /items/1/edit    GET
    def edit                                             #редоктируем атрибты нашего товара
-     @item = Item.find(params[:id])
+  #   @item = Item.find(params[:id])
    end
 
    #     /items/1        DELETE
       def destroy
-        @item = Item.find(params[:id])
+    #    @item = Item.find(params[:id])
         @item.destroy
         redirect_to action: "index"
       end
+
+  #метод для сохранения параметров в БД
+private
+      def item_params
+         params.require(:item).permit(:price, :name, :weight, :description)
+      end
+
+      def find_item
+        @item = Item.find(params[:id])
+      end
+
+      def check_if_admin
+        render html: "Access denied", status: 403 unless params [:admin]
+      end
+    end
  end
