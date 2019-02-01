c166403ad0ddd34afbfffd2371846310f605a2f7
Oleg <pavlenko111@i.ua>
2019-02-01 20:39:17 +0200

    config

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 
 module MyStore
   class Application < Rails::Application
-
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     #config.autoload_paths += %W(#{config.root}/lib)
