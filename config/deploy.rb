ff5b563192c795b65f972b819530f315c8bf80a2
Oleg <pavlenko111@i.ua>
2019-02-27 17:19:00 +0200

    Cap ready

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:OlegDev11/my_store.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/var/www/my_store"
+set :deploy_to, "/home/deploy/apps/my_store"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
